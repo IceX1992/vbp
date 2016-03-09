0b6053f6b5df26b1d72c7fd740d87f0076a26e01
IceX1992 <dioncloor@hotmail.com>
2016-03-09 10:52:58 -0300

    Initial commit

---
+++ app/src/main/java/beroepsproduct/dion/test/com/beroepsproduct/LoginScherm.java
@@ -0,0 +1,84 @@
+package beroepsproduct.dion.test.com.beroepsproduct;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginScherm extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_scherm);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void onClickLogin(View view){
+        EditText username =(EditText) findViewById(R.id.username);
+        String usernameValue = String.valueOf(username.getText());
+
+        EditText password = (EditText) findViewById(R.id.password);
+        String passwordValue = String.valueOf(password.getText());
+
+        String notification = "";
+
+        //change to !usernameValue.isEmpty() && !passwordValue.isEmpty()
+        if (usernameValue.equals("dion") && passwordValue.equals("dion")){
+        //db authentication
+            onClick(usernameValue);
+            notification = "Welkom " + usernameValue;
+        }else{
+            notification = "Foutieve inlog gegevens, probeert U het nog een keer";
+        }
+        Toast.makeText(this,notification,Toast.LENGTH_SHORT).show();
+    }
+
+    public void onClick(String username){
+        Intent intent = new Intent(this,Dashboard.class);
+       // String welcomeMessage = "Welcome " + username;
+       // intent.putExtra("message", welcomeMessage);
+        startActivity(intent);
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login_scherm, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
