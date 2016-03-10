e788b2a30d426c326ca1fd069055ed5048a63357
IceX1992 <dioncloor@hotmail.com>
2016-03-10 13:45:08 -0300

    db added

---
+++ app/src/main/java/beroepsproduct/dion/test/com/beroepsproduct/database/MainDAO.java
@@ -0,0 +1,29 @@
+package beroepsproduct.dion.test.com.beroepsproduct.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by Dion on 3/10/2016.
+ */
+public class MainDAO extends SQLiteOpenHelper {
+
+    private static final String DATABASE_NAME = "verzekeringen_users.db";
+    private static final int DATABASE_VERSION = 1;
+
+
+    public MainDAO(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
