bc876c131939db34ffea13503f5519adb2a4f9e9
MyloMantis <justinkareem@live.com>
2016-03-11 17:52:17 -0300

    Buttons toegevoegd

--- app/src/main/java/beroepsproduct/dion/test/com/beroepsproduct/Dashboard.java
+++ app/src/main/java/beroepsproduct/dion/test/com/beroepsproduct/Dashboard.java
@@ -40,14 +40,7 @@ public class Dashboard extends AppCompatActivity {
         db = new MainDAO(this);
         // onArrive();
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     @Override
