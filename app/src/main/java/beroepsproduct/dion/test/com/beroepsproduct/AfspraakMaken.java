14788ee80cfaca6fed697b04c56462c8cf2d9f13
IceX1992 <dioncloor@hotmail.com>
2016-03-11 18:22:07 -0300

    calenderview gemaakt, afspraak geklikt en terug naar dashboard

--- app/src/main/java/beroepsproduct/dion/test/com/beroepsproduct/AfspraakMaken.java
+++ app/src/main/java/beroepsproduct/dion/test/com/beroepsproduct/AfspraakMaken.java
@@ -1,12 +1,12 @@
 package beroepsproduct.dion.test.com.beroepsproduct;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.CalendarView;
+import android.widget.Toast;
 
 public class AfspraakMaken extends AppCompatActivity {
 
@@ -18,21 +18,50 @@ public class AfspraakMaken extends AppCompatActivity {
         setContentView(R.layout.activity_afspraak_maken);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        afspraak = (CalendarView) findViewById(R.id.calendarView);
 
-   /*     afspraak.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                Toast.makeText(getApplicationContext(),
-                        "Year: " + year + "\n" +
-                                "Month: " + month + "\n" +
-                                "Day of Month: " + dayOfMonth,
-                        Toast.LENGTH_LONG).show();
-            }
-        });
-        */
-
+        initializeCalender();
 
     }
 
+
+    public void initializeCalender() {
+        afspraak = (CalendarView) findViewById(R.id.calendarView);
+
+        //sets whether to show the week number
+        afspraak.setShowWeekNumber(false);
+
+        //sets the first day of the week according to Calendar
+        //here we set Monday as the first day of the Calendar
+        afspraak.setFirstDayOfWeek(2);
+
+        //the backgroundcolor for the selected week
+        afspraak.setSelectedWeekBackgroundColor(getResources().getColor(R.color.green));
+
+        //sets the color for the dates of an unfocused month
+        afspraak.setUnfocusedMonthDateColor(getResources().getColor(R.color.transparent));
+
+        //sets the color for the separator line between weeks.
+        afspraak.setWeekSeparatorLineColor(getResources().getColor(R.color.transparent));
+
+        //sets the color for the vertical bar shown at the beginning and at the end of the selected date.
+        afspraak.setSelectedDateVerticalBar(R.color.darkgreen);
+
+        //sets the listener to be notified upon selected date change.
+        afspraak.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void back2DB2(View view) {
+        intentDB();
+    }
+
+    public void intentDB() {
+        Intent intent = new Intent(this, Dashboard.class);
+        startActivity(intent);
+
+    }
 }
