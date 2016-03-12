7016bb2c984aed4a6ddd385446ce6da54cad6767
MyloMantis <justinkareem@live.com>
2016-03-12 16:30:59 -0300

    Buttons toegevoegd

--- app/src/main/java/beroepsproduct/dion/test/com/beroepsproduct/AfspraakMaken.java
+++ app/src/main/java/beroepsproduct/dion/test/com/beroepsproduct/AfspraakMaken.java
@@ -12,50 +12,29 @@ public class AfspraakMaken extends AppCompatActivity {
 
     CalendarView afspraak;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_afspraak_maken);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        initializeCalender();
-
-    }
 
 
-    public void initializeCalender() {
+
+
         afspraak = (CalendarView) findViewById(R.id.calendarView);
-
-        //sets whether to show the week number
-        afspraak.setShowWeekNumber(false);
-
-        //sets the first day of the week according to Calendar
-        //here we set Monday as the first day of the Calendar
-        afspraak.setFirstDayOfWeek(2);
-
-        //the backgroundcolor for the selected week
-        afspraak.setSelectedWeekBackgroundColor(getResources().getColor(R.color.green));
-
-        //sets the color for the dates of an unfocused month
-        afspraak.setUnfocusedMonthDateColor(getResources().getColor(R.color.transparent));
-
-        //sets the color for the separator line between weeks.
-        afspraak.setWeekSeparatorLineColor(getResources().getColor(R.color.transparent));
-
-        //sets the color for the vertical bar shown at the beginning and at the end of the selected date.
-        afspraak.setSelectedDateVerticalBar(R.color.darkgreen);
 
         //sets the listener to be notified upon selected date change.
         afspraak.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + month + "/" + year, Toast.LENGTH_LONG).show();
+            public void onSelectedDayChange(CalendarView view, int year, int month  , int dayOfMonth) {
+                Toast.makeText(getApplicationContext(), dayOfMonth + "/" + (month+1) + "/" + year, Toast.LENGTH_LONG).show();
             }
         });
     }
 
     public void back2DB2(View view) {
+        String notification= "Uw afspraak is gemaakt";
+        Toast.makeText(this,notification,Toast.LENGTH_SHORT).show();
         intentDB();
     }
 
@@ -64,4 +43,5 @@ public class AfspraakMaken extends AppCompatActivity {
         startActivity(intent);
 
     }
+
 }
