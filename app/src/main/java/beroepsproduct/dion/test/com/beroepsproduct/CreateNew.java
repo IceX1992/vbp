b2a01f96cf1760814a4a27fa39aeb958ff3c5c9f
MyloMantis <justinkareem@live.com>
2016-03-13 16:10:25 -0300

    3 Buttons toegevoegd, "Registreer" en " Terug naar Login" , wijziging op Create New activity

--- app/src/main/java/beroepsproduct/dion/test/com/beroepsproduct/CreateNew.java
+++ app/src/main/java/beroepsproduct/dion/test/com/beroepsproduct/CreateNew.java
@@ -27,14 +27,7 @@ public class CreateNew extends AppCompatActivity {
 
         db = new MainDAO(this);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
     }
 
     public void createUser(View view) {
